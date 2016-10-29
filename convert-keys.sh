a5126f2acfd7d53e28742484353f0c12e983dc1b
brandenbyers <branden@brandenbyers.com>
2016-10-28 21:12:53 -0700

    Add separateKeyboards functionality

--- convert-keys.sh
+++ convert-keys.sh
@@ -3,26 +3,28 @@
 const fs = require('fs')
 const args = require('minimist')(process.argv.slice(2))
 const path = require('path')
+const ent = require('ent')
 
 const dotPath = args.dot
 const dotFile = fs.readFileSync(dotPath, "utf8")
 
-var layersAndKeys = {}
+var layers = {}
+
 
 function collectLayers(dotFile) {
-  const layers = dotFile.match(/\*\s\w+\sLAYER \*/gi)
-  console.log(layers)
+  const layerMatches = dotFile.match(/\*\s\w+\sLAYER \*/gi)
+  console.log(layerMatches)
   let layerBlockIndexes = []
   let layerBlocks = []
   let layerName = ""
-  if (layers) {
-    layers.forEach(function(el) {
+  if (layerMatches) {
+    layerMatches.forEach(function(el) {
       layerBlockIndexes.push(dotFile.indexOf(el))
     })
     layerBlockIndexes.forEach(function(el, i) {
-      layerName = layers[i].toString().match(/\*\s(\w+)\sLAYER \*/)[1].toLowerCase()
+      layerName = layerMatches[i].toString().match(/\*\s(\w+)\sLAYER \*/)[1].toLowerCase()
       console.log(layerName)
-      layersAndKeys[layerName] = {}
+      layers[layerName] = {}
       if (i < layerBlockIndexes.length) {
         return layerBlocks.push([layerName, dotFile.substring(el, layerBlockIndexes[i + 1])])
       } else {
@@ -30,26 +32,53 @@ function collectLayers(dotFile) {
       }
     })
     console.log("Indexes:", layerBlockIndexes)
-    console.log(layersAndKeys)
+    console.log(layers)
   }
   return layerBlocks
 }
 
 function collectKeys(layerBlocks) {
   return layerBlocks.forEach(function(el, i) {
-    layersAndKeys[el[0]].keys = el[1].match(/<td.+>(.+)<\/font><\/td>/gi)
-    return layersAndKeys[el[0]].keys.forEach(function(el, i, arr) {
-      // TODO: replace any HTML symbol codes
+    layers[el[0]].keys = el[1].match(/<td.+>(.+)<\/font><\/td>/gi)
+    return layers[el[0]].keys.forEach(function(el, i, arr) {
       // TODO: flag any modifiers that will require manual key decisions or figure out commenting system and grab comments instead key name visible in the pdf
-      return arr[i] = el.match(/<td.+>(.+)<\/font><\/td>/i)[1]
+      return arr[i] = ent.decode(el.match(/<td.+>(.+)<\/font><\/td>/i)[1])
     })
   })
 }
 
+function separateKeyboards(layers) {
+  if (!layers) {
+    return console.log("Can not separate rows without layers and keys.")
+  }
+  for (let layer in layers) {
+    // Define row arrays
+    layers[layer].rows = {}
+    for (let rowNumber of [0,1,2,3,4,5,6,7,8,9]){
+      layers[layer].rows[rowNumber] = []
+    }
+    // Split vertical keyboard into horizontal rows
+    layers[layer].keys.forEach(function(key, i) {
+      let rowNumber = i % 10
+      console.log('Row number', rowNumber)
+      layers[layer].rows[rowNumber].unshift(key)
+    })
+    // Define keyboard left and keyboard right
+    let rows = layers[layer].rows
+    console.log("rowy", rows)
+    console.log("wozer", rows[0])
+    layers[layer].leftKeyboard = [].concat(rows["0"], rows["1"], rows["2"], rows["3"], rows["4"])
+    layers[layer].rightKeyboard = [].concat(rows["5"], rows["6"], rows["7"], rows["8"], rows["9"])
+  }
+}
+
 const layerBlocks = collectLayers(dotFile)
 collectKeys(layerBlocks)
-console.log(layersAndKeys)
+separateKeyboards(layers)
+console.log(layers)
 
 // TODO: split keys by keyboard half
 // TODO: separate keys into proper horizontally oriented keyboard rows
 // TODO: write keys to QMK file
+//
+// If modulo unshift specific left/right keyboard and specific row 1-5
