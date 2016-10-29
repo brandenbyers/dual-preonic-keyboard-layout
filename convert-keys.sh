45056480c794aaf3f576c6de68f046790b021fa3
brandenbyers <branden@brandenbyers.com>
2016-10-28 21:35:38 -0700

    Add QMK keyboard string

--- convert-keys.sh
+++ convert-keys.sh
@@ -72,13 +72,25 @@ function separateKeyboards(layers) {
   }
 }
 
+function keyboardToQMKString(layer, keyboard) {
+  // layer = base_right
+  let k = keyboard
+  str = `[${layer}] = {
+  {${k[0]},  ${k[1]},  ${k[2]},  ${k[3]},  ${k[4]},  ${k[5]},  ${k[6]},  ${k[7]},  ${k[8]},  ${k[9]},  ${k[10]},  ${k[11]}},
+  {${k[12]},  ${k[13]},  ${k[14]},  ${k[15]},  ${k[16]},  ${k[17]},  ${k[18]},  ${k[19]},  ${k[20]},  ${k[21]},  ${k[22]},  ${k[23]}},
+  {${k[24]},  ${k[25]},  ${k[26]},  ${k[27]},  ${k[28]},  ${k[29]},  ${k[30]},  ${k[31]},  ${k[32]},  ${k[33]},  ${k[34]},  ${k[35]}},
+  {${k[36]},  ${k[37]},  ${k[38]},  ${k[39]},  ${k[40]},  ${k[41]},  ${k[42]},  ${k[43]},  ${k[44]},  ${k[45]},  ${k[46]}}, ${k[47]}},
+  {${k[48]},  ${k[49]},  ${k[50]},  ${k[51]},  ${k[52]},  ${k[53]},  ${k[54]},  ${k[55]},  ${k[56]},  ${k[57]},  ${k[58]}}, ${k[59]}}
+},`
+  console.log(str)
+}
+
 const layerBlocks = collectLayers(dotFile)
 collectKeys(layerBlocks)
 separateKeyboards(layers)
 console.log(layers)
+keyboardToQMKString('_BASE_LEFT', layers.base.leftKeyboard)
 
-// TODO: split keys by keyboard half
-// TODO: separate keys into proper horizontally oriented keyboard rows
 // TODO: write keys to QMK file
 //
 // If modulo unshift specific left/right keyboard and specific row 1-5
